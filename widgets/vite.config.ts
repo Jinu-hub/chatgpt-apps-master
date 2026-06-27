0b488cf28b132efeca0efe7d099613f8da68760e
Jinwoo's mac air <jinu30dev@gmail.com>
2026-06-27 11:53:54 +0900

    UI Tour

--- widgets/vite.config.ts
+++ widgets/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-const FULL_URL = "https://workouts-server.jinu30dev.workers.dev/";
+const FULL_URL = "https://ecommerce-server.jinu30dev.workers.dev/";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
